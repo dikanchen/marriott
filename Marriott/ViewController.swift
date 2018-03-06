e7f648de192be74049e8de377267e38eb8582051
User <user@aca814fc.ipt.aol.com>
2018-03-06 10:04:35 -0500

    Initial Commit

---
+++ Marriott/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Marriott
+//
+//  Created by User on 3/6/18.
+//  Copyright © 2018 dikan.chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
